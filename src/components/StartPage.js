8647b96c7b1494866a205ce6fc28a7e820b4d376
Nathan Atherton <nathanatherton2@gmail.com>
2020-05-27 12:25:14 +0100

    Capitalised the difficulty options

--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -5,9 +5,9 @@ const StartPage = props => {
   return (
     <>
       <h3>Choose your difficulty</h3>
-      <Button wording="easy" origin="start" setDifficulty={props.setDifficulty}/>
-      <Button wording="medium" origin="start" setDifficulty={props.setDifficulty}/>
-      <Button wording="hard" origin="start" setDifficulty={props.setDifficulty}/>
+      <Button wording="Easy" origin="start" setDifficulty={props.setDifficulty}/>
+      <Button wording="Medium" origin="start" setDifficulty={props.setDifficulty}/>
+      <Button wording="Hard" origin="start" setDifficulty={props.setDifficulty}/>
     </>
   )
 }
