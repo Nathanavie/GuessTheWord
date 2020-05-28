a659c3d43f03ec48c97dee38c0d692fe40343ed5
Nathan Atherton <nathanatherton2@gmail.com>
2020-05-28 15:13:51 +0100

    Stopped anyone adding a score of 0 to the leaderboard

--- src/components/EndOfGame.js
+++ src/components/EndOfGame.js
@@ -3,15 +3,24 @@ import Button from './Button';
 import AddUserScore from './AddUserScore';
 
 const EndOfGame = props => {
-  return (
-    <>
-      <h3>Your Final Score was {props.finalScore}</h3>
-      <Button wording="Start Again" origin="EndOfGame" restart={props.restartGame} />
-      <div className="addScoreContainer">
-        <h4>Want to add your score to the leaderboard?</h4>
-        <AddUserScore score={props.finalScore} addScore={props.addScore}/>
-      </div>
-    </>
-  )
+  if (props.finalScore === 0) {
+    return (
+      <>
+        <h3>Your Final Score was {props.finalScore}</h3>
+        <Button wording="Start Again" origin="EndOfGame" restart={props.restartGame} />
+      </>
+    )
+  } else {
+    return (
+      <>
+        <h3>Your Final Score was {props.finalScore}</h3>
+        <Button wording="Start Again" origin="EndOfGame" restart={props.restartGame} />
+        <div className="addScoreContainer">
+          <h4>Want to add your score to the leaderboard?</h4>
+          <AddUserScore score={props.finalScore} addScore={props.addScore}/>
+        </div>
+      </>
+    )
+  }
 }
 export default EndOfGame
